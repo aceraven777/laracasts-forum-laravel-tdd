8dda3388fb0dcf5bdeebb4a1dbc5ffc4edd144d7
Aristheo Lacdao <aceraven777@gmail.com>
2018-07-24 11:33:24 +0800

    Episode 32: Edit reply, use Reply component

--- tests/Feature/ParticipateInThreadsTest.php
+++ tests/Feature/ParticipateInThreadsTest.php
@@ -4,7 +4,7 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 
-class ParticipateInForumTest extends TestCase
+class ParticipateInThreadsTest extends TestCase
 {
     /** @test */
     function unauthenticated_users_may_not_add_replies()
@@ -62,6 +62,41 @@ class ParticipateInForumTest extends TestCase
         $this->delete("replies/{$reply->id}")->assertStatus(302);
 
         $this->assertDatabaseMissing('replies', ['id' => $reply->id]);
+    }
+
+    function unauthorized_users_cannot_update_replies()
+    {
+        $this->withExceptionHandling();
+        
+        $reply = make('App\Reply');
+        
+        $this->patch('/replies/' . $reply->id, [
+            'body' => 'This is a new reply',
+        ])->assertRedirect('login');
+
+        $user = create('App\User');
+        $this->signIn($user);
+
+        $this->patch('/replies/' . $reply->id, [
+            'body' => 'This is a new reply',
+        ])->assertStatus(403);
+    }
+
+    function authorized_users_can_update_replies()
+    {
+        $user = create('App\User');
+        $this->signIn($user);
+        $reply = make('App\Reply', ['user_id' => $user->id]);
+
+        $updatedReply = 'This is a new reply';
+        $this->patch('/replies/' . $reply->id, [
+            'body' => $updatedReply,
+        ]);
+
+        $this->assertDatabaseHas('replies', [
+            'id' => $reply->id,
+            'body' => $updatedReply,
+        ]);
     }
 
     function publishReply($overrides)
