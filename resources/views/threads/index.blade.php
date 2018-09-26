1e0b683a1a75b631b29b62c3134fdc620a91adbf
Aristheo Lacdao <aceraven777@gmail.com>
2018-09-26 11:06:03 +0800

    Episode 66 - trending threads with redis

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -3,10 +3,32 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-8">
             @include('threads._list')
 
             {{ $threads->render() }}
+        </div>
+
+        <div class="col-md-4">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    Trending Threads
+                </div>
+
+                <div class="panel-body">
+                    @if (count($trending))
+                        <ul class='list-group'>
+                            @foreach ($trending as $thread)
+                                <li class='list-group-item'>
+                                    <a href="{{ url($thread->path) }}">{{ $thread->title }}</a>
+                                </li>
+                            @endforeach
+                        </ul>
+                    @else
+                        <p>No trending threads to show.</p>
+                    @endif
+                </div>
+            </div>
         </div>
     </div>
 </div>
