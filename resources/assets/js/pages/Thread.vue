78b53cd888a81a53e35baf6d9c2cdd76f76a4900
Aristheo Lacdao <aceraven777@gmail.com>
2018-10-16 13:33:48 +0800

    Episode 93 - A thread cana be updated (AJAX)

--- resources/assets/js/pages/Thread.vue
+++ resources/assets/js/pages/Thread.vue
@@ -12,20 +12,57 @@
                 repliesCount: this.thread.replies_count,
                 locked: this.thread.locked,
                 editing: false,
+                title: this.thread.title,
+                body: this.thread.body,
+                form: {},
             };
+        },
+
+        created () {
+            this.resetForm();
         },
 
         methods: {
             toggleLock() {
+                let uri = '/locked-threads/' + this.thread.slug;
                 let method = (this.locked ? 'delete' : 'post');
                 
-                axios[method]('/locked-threads/' + this.thread.slug)
+                axios[method](uri)
 				.catch((error) => {
                     flash(error.response.data, 'danger');
                 })
 				.then(({data}) => {
 					this.locked = ! this.locked;
 				});
+            },
+
+            update() {
+                let uri = '/threads/' + this.thread.channel.slug + '/' + this.thread.slug;
+                
+                axios.patch(uri, this.form).then(() => {
+					this.title = this.form.title;
+					this.body = this.form.body;
+
+					this.editing = false;
+
+                    flash('Your thread has been updated.');
+				}).catch(function (error) {
+					if (error.response.status == 403) {
+						flash('Your action is unauthorized.', 'danger');
+						return;
+					}
+
+					flash('An error has occurred.', 'danger');
+				});
+            },
+
+            resetForm() {
+                this.form = {
+                    title: this.title,
+                    body: this.body,
+                };
+
+                this.editing = false;
             },
         },
     }
